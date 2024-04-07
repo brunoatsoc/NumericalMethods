0b9613714595915ce871eeb4f46ab2c806ee6247
brunoatsoc <brunosantos20003237@gmail.com>
2024-04-07 16:18:47 -0300

    First methods

---
+++ First/LUfactorization/lufactorization.py
@@ -0,0 +1,103 @@
+# Metodo para trocar uma linha por outra caso o pivor de uma das linhas da array seja zero
+def replaceline(array, line1, line2):
+    array[line1], array[line2] = array[line2], array[line1]
+
+    # Retorna o array
+    return array
+
+# Metodo da eliminação de Gauss
+def gausselimination(array):
+    size = len(array) # Tamanho do array(número de linhas)
+    # Contadores para linhas e colunas
+    i = 0
+    j = 0
+    mtpl = []
+
+    # Loop para alterar as linhas da matriz
+    while(j < size):
+        pivot = array[i][i] # Pivor da matriz
+
+        # Caso o pivor seja zero, a linha em que ele está sera trocada
+        if(pivot == 0):
+            k = i
+
+            # Procura uma linha em que o pivor não seja zero
+            while(k < size):
+                if array[k][i] != 0.0:
+                    break
+                k = k + 1
+
+            array = replaceline(array, k, i) # Pega a nova matriz com as linhas trocadas
+
+        j = i + 1
+
+        k = j # Contador para as linhas
+
+        # Loop para zerar a coluna abaixo do pivor
+        while(k < size):
+            multiplicator = array[k][i] / pivot
+            mtpl.append(multiplicator)
+
+            arraytemp = [element * multiplicator for element in array[i]]
+            array[k] = [a - b for a, b in zip(array[k], arraytemp)]
+            k = k + 1
+
+        i = i + 1
+        
+    return array, mtpl # Retorna o array escalonado
+
+array = [] # Array onde vai ser guardada a matriz
+multiplicator = []
+b = []
+
+# Abre o aquivo em que está a matriz e guarda na variavel array
+with open("lufactorization.txt", 'r') as file:
+    linhas = file.readlines()
+
+    # Encontrar a quebra de linha
+    idx_quebra = linhas.index('\n')
+
+    # Ler a matriz
+    for linha in linhas[:idx_quebra]:
+        elementos = linha.strip().split(' ')
+        array.append([float(elemento) for elemento in elementos if elemento])
+
+    # Ler os valores após a quebra de linha
+    for linha in linhas[idx_quebra+1:]:
+        b.extend([float(elemento) for elemento in linha.strip().split(' ') if elemento])
+
+array, multiplicator = gausselimination(array) # Array atualizado com a matriz escalonada
+
+size = len(array)
+l = [[None] * size for _ in range(size)]
+i = 0
+k = 0
+while(i < size):
+    j = 0
+    while(j < size):
+        if i == j:
+            l[i][j] = 1
+        elif i > j:
+            l[i][j] = multiplicator[k]
+            k = k + 1
+        else:
+            l[i][j] = 0
+        j = j + 1
+    i = i + 1
+
+# Cria um arquivo com a matriz escalonada e com a resposta
+with open("xxxxx.txt", 'w') as file:
+    # Escreve a matriz no arquivo
+    for line in array:
+        line_format = ' '.join(map(str, line))
+        file.write(line_format + '\n')
+    
+    file.write('\n')
+    
+    for line in l:
+        line_format = ' '.join(map(str, line))
+        file.write(line_format + '\n')
+    
+    file.write('\n')
+    
+    file.write(f'B = {b}')
